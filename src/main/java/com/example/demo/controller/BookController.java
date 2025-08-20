c158c37c2dea1dd44142063b6e3b608db99df82d
MartaOsuchaMazur <jagooo1@wp.pl>
2025-08-20 21:26:21 +0200

    project refactor

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,6 +1,7 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 
+import com.example.demo.entity.Book;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
