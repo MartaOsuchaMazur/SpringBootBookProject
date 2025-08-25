7df7397c7a5f4ace8bedfab55e4c8ea450cf62ca
MartaOsuchaMazur <jagooo1@wp.pl>
2025-08-25 18:12:49 +0200

    modyfied dependencies

---
+++ src/test/java/com/example/demo/controller/BookControllerMockMVCTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookControllerMockMVCTest {
+
+    @Test
+    void getBooks() {
+    }
+
+    @Test
+    void getBookById() {
+    }
+
+    @Test
+    void addBook() {
+    }
+
+    @Test
+    void updateBook() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+
+    @Test
+    void searchBooks() {
+    }
+}
