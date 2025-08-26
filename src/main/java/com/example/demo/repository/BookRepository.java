35ea84d993eeacc24c813a6d8b9f56e202769d4e
MartaOsuchaMazur <jagooo1@wp.pl>
2025-08-26 21:59:41 +0200

    modified the BookRepository

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.repository;
 
-public interface BookRepository {
+import com.example.demo.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
