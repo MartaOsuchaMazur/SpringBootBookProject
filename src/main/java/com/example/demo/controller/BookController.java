29463b5e752e9d8045a180be615f18c75d7c36c8
MartaOsuchaMazur <jagooo1@wp.pl>
2025-08-26 21:54:26 +0200

    added old version of controller, modyfi BookController and added templates

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,63 +1,64 @@
 package com.example.demo.controller;
 
-
 import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/books")
 public class BookController {
 
-    private final List<Book> books = new ArrayList<>();
+    private final BookRepository bookRepository;
 
-    public BookController() {
-        books.add(new Book(1L, "Hobbit", "J.R.R. Tolkien"));
-        books.add(new Book(2L, "1984", "George Orwell"));
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
 
     @GetMapping("")
-    public List<Book> getBooks(Model model) {
-        model.addAttribute("books", books);
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
         return "book_list";
     }
 
-    @GetMapping("/{id}")
-    public Book getBookById(@PathVariable Long id) {
-        return books.stream()
-                .filter(book -> book.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-    }
-    @PostMapping
-    public Book addBook(@RequestBody Book book){
-        books.add(book);
-        return book;
-    }
-    @PutMapping("/{id}")
-    public Book updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
-        Book book = books.stream()
-                .filter(b ->b.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-        if (book != null) {
-            book.setTitle(updatedBook.getTitle());
-            book.setAuthor(updatedBook.getAuthor());
-        }
-        return book;
+    @GetMapping("/add")
+    public String showFormToAddBook(Model model){
+      model.addAttribute("book", new Book());
+      return "add_book";
     }
 
-    @DeleteMapping("/{id}")
+    @PostMapping ("/add")
+    public String addBook(@ModelAttribute Book book){
+        bookRepository.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showFormToEditBook(@PathVariable Long id, Model model){
+        Book book = bookRepository.findById(id).orElse(null);
+        model.addAttribute("book", book);
+        return "edit_book";
+    }
+    @PostMapping("edit/{id}")
+    public String editBook(@PathVariable Long id, @ModelAttribute Book book) {
+        book.setId(id);
+        bookRepository.save(book);
+        return "redirect:/books";
+    }
+
+    @PostMapping("delete/{id}")
     public String deleteBook(@PathVariable Long id) {
-        books.removeIf(book -> book.getId().equals(id));
-        return "Book with Id " + id + " removed";
+        bookRepository.deleteById(id);
+        return "redirect:/books";
     }
 
-    @GetMapping("/search")
-    public List<Book> searchBooks(@RequestParam String author){
-    return books;
-    }
+//    @GetMapping("/search")
+//    public String searchBooks(@RequestParam String author){
+//        bookRepository.findByQuery(author);
+//    return "redirect:/books";
+//    }
 }
---
+++ src/main/java/com/example/demo/controller/BookController3.java
@@ -0,0 +1,67 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.entity.Book;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/books")
+public class BookController3 {
+
+//    private final List<Book> books = new ArrayList<>();
+//
+//    public BookController3() {
+//        books.add(new Book(1L, "Hobbit", "J.R.R. Tolkien"));
+//        books.add(new Book(2L, "1984", "George Orwell"));
+//        books.add(new Book(3L, "Wiedzmin", "Andrzej Sapkowski"));
+//        books.add(new Book(4L, "Lord of the Rings", "J.R.R. Tolkien"));
+//    }
+//
+//    @GetMapping("")
+//    public List<Book> getBooks(Model model) {
+//        model.addAttribute("books", books);
+//        return "book_list";
+//    }
+//
+//    @GetMapping("/{id}")
+//    public Book getBookById(@PathVariable Long id) {
+//        return books.stream()
+//                .filter(book -> book.getId().equals(id))
+//                .findFirst()
+//                .orElse(null);
+//    }
+//
+//    @PostMapping("/add")
+//    public String addBook(@RequestBody Book book){
+//        books.add(book);
+//        return book;
+//    }
+//
+//    @PutMapping("/{id}")
+//    public Book updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
+//        Book book = books.stream()
+//                .filter(b ->b.getId().equals(id))
+//                .findFirst()
+//                .orElse(null);
+//        if (book != null) {
+//            book.setTitle(updatedBook.getTitle());
+//            book.setAuthor(updatedBook.getAuthor());
+//        }
+//        return book;
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public String deleteBook(@PathVariable Long id) {
+//        books.removeIf(book -> book.getId().equals(id));
+//        return "Book with Id \" + id + \" removed";
+//    }
+//
+//    @GetMapping("/search")
+//    public List<Book> searchBooks(@RequestParam String author){
+//    return books;
+//    }
+}
