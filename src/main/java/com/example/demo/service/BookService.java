5d495a127301e26a100a3a298ad2bdfaca6a19e5
MartaOsuchaMazur <jagooo1@wp.pl>
2025-08-26 22:35:19 +0200

    added BookService

---
+++ src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> finAll(){
+        return bookRepository.findAll();
+    }
+
+    public Optional<Book> findById(Long id){
+        return bookRepository.findById(id);
+    }
+
+    public void save(Book book){
+        bookRepository.save(book);
+    }
+
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
+    public void deleteAll(){
+        bookRepository.deleteAll();
+    }
+}
